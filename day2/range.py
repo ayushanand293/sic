82d46af0dc201767c5233dbe98e840eb69ecf067
ayushanand293 <ayushanand293@gmail.com>
2025-02-18 11:02:38 +0530

    range function successfully imitated

--- day2/range.py
+++ day2/range.py
@@ -1,4 +1,25 @@
-input_number = int(input('Enter a small number of your choice: '))
+def my_range(*args):
+    if len(args) == 1:
+        i=0
+        while i < args[0]:
+            yield i
+            i += 1
+    elif len(args) == 2:
+        i = args[0]
+        while i < args[1]:
+            yield i
+            i += 1
+    elif len(args) == 3:
+        i = args[0]
+        if args[0] <= args[1]  and args[-1] > 0:
+            while i < args[1]:
+                yield i 
+                i += args[-1]
+        if args[0] >= args[1]  and args[-1] < 0:
+            while i > args[1]:
+                yield i 
+                i += args[-1]        
 
-for i in range(input_number):
-    print(i)
+
+for i in my_range(80, 40, -4):
+    print(i, end = '  ')
